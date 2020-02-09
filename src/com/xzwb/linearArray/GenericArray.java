f2ef3be75332ec8d4cd8ee17e69f997a32ed5592
xzwb <1478863479@qq.com>
2020-02-09 16:22:28 +0800

    质的飞跃

---
+++ src/com/xzwb/linearArray/GenericArray.java
@@ -0,0 +1,85 @@
+package com.xzwb.linearArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class GenericArray<T extends Comparable> {
+    private int size;
+    private T[] array;
+    private int length;
+    private final int initSize = 4;
+
+    /**
+     * 有参构造
+     * @param size
+     */
+    public GenericArray(int size) {
+        if (size < initSize) {
+            array = (T[])(new Comparable[initSize]);
+        } else {
+            array = (T[])(new Comparable[size]);
+        }
+        length = 0;
+        this.size = size;
+    }
+
+    /**
+     * 无参构造
+     */
+    public GenericArray() {
+        array = (T[])(new Comparable[initSize]);
+        size = initSize;
+        length = 0;
+    }
+
+    /**
+     * 数组的拷贝
+     * @param ts
+     * @return
+     */
+    private T[] arrayCopy(T[] ts) {
+        for (int i = 0; i < length; i++) {
+            ts[i] = array[i];
+        }
+        return ts;
+    }
+
+    /**
+     * 数组的扩容
+     */
+    private void expand() {
+        if (length == size) {
+            size <<= 1;
+            array = arrayCopy((T[])(new Object[size]));
+        }
+    }
+
+    /**
+     * 添加元素
+     * @param ele
+     */
+    public void add(T ele) {
+        expand();
+        array[length] = ele;
+        length++;
+    }
+
+    @Override
+    public String toString() {
+        return "GenericArray{" +
+                "array=" + Arrays.toString(array) +
+                '}';
+    }
+
+    public T[] getArray() {
+        return array;
+    }
+
+    public void setArray(T[] array) {
+        this.array = array;
+    }
+
+    public <T> void sort() {
+        array[0] = array[1];
+    }
+}
---
+++ src/com/xzwb/test/GenericArrayTest.java
@@ -0,0 +1,13 @@
+package com.xzwb.test;
+
+import com.xzwb.linearArray.GenericArray;
+
+public class GenericArrayTest {
+    public static void main(String[] args) {
+        GenericArray array = new GenericArray();
+        array.add(1);
+        array.add(2);
+        array.sort();
+        System.out.println(array);
+    }
+}
